953f70e4a24aaed2d476fac7b08ddc8367d4cf14
Okechukwu Ochia <emmirald@gmail.com>
2018-02-19 00:41:34 -0700

    comparison operator for numerical operators

---
+++ more_conditional_operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// exampe on conditional operators
+
+int main()
+{
+int a,b,c;
+a = 2;
+b = 3;
+
+c = (a>b) ? a : b;
+
+cout << "the value of c is: " <<c <<'\n';
+
+
+
+
+
+
+
+
+}
