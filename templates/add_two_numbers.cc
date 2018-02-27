8f8f07d40e604f221b653e88f4fbb03f52a36c5b
Okechukwu Ochia <emmirald@gmail.com>
2018-02-26 23:38:34 -0700

    nothing much here, just making the code more compact

--- templates/add_two_numbers.cc
+++ templates/add_two_numbers.cc
@@ -2,15 +2,14 @@
 using namespace std;
 int main(){
 int a, b, sum, diff; //declare four int variables in memory
-printf("Enter the first number please: ");
+cout << "Please enter the first number: ";
 cin >>  a;  //enter the value of each variable
-printf("Enter the second number please: ");
+cout << "Please enter the second number: ";
 cin >>  b;
-sum = a + b;  // compute the sum of the two inputed variables and save in the third
-diff = a - b;  //compute the difference between the two inputed variables and save in the fourth.
+sum = a + b;  //compute the sum of the two inputed variables and save in the third
+diff = a - b; //compute the difference between the two inputed variables and save in the fourth.
 
-printf("The sum and difference of both numbers are respectively: ");
-cout << sum << " and " <<diff << '\n';
+cout << "The sum and difference of both numbers is respectively "<<sum << " and " <<diff << '\n';
 
 return 0;
 }
