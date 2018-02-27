07160d385cdbb5b5536beaac365f6d8847c2c9ef
Okechukwu Ochia <emmirald@gmail.com>
2018-02-26 21:40:08 -0700

    added code for I/O and computing the difference between the input variables

--- templates/add_two_numbers.cc
+++ templates/add_two_numbers.cc
@@ -1,7 +1,17 @@
 #include <iostream>
-using  namespace std;
-int a, b, sum;
-std::cin >> a;
-std:: cin >> b;
-sum = a + b;
-std ::cout << sum << endl
+using namespace std;
+int main(){
+int a, b, sum, diff; //declare four int variables in memory
+printf("Enter the first number please: ");
+cin >>  a;  //enter the value of each variable
+printf("Enter the second number please: ");
+cin >>  b;
+sum = a + b;  // compute the sum of the two inputed variables and save in the third
+diff = a - b;  //compute the difference between the two inputed variables and save in the fourth.
+
+printf("The sum and difference of both numbers are respectively: ");
+cout << sum << ',';
+cout << diff << '\n';
+
+return 0;
+}
