9404befc18548bf3f014ff6067a8e0d6548434a8
Okechukwu Ochia <emmirald@gmail.com>
2018-04-22 01:48:41 -0600

    Added an extra logic to query the customer's price range

--- templates/data_struct.cc
+++ templates/data_struct.cc
@@ -19,18 +19,19 @@ member_type3 member_name3;
 
  **/
 struct phone{
-	string model;
-	string platform_version;
+	string model; // smartphone model
+	string platform_version; // operating system platform and version tag
 	int weight;  // weight in ounces
 	double price; // price in CAD
 };
 phone iPhone;
 phone Samsung;
+phone Blackberry;
 
 
  int main(){
-
-iPhone.model = "X";
+//objects of the previously declared struct and their associated members/properties
+iPhone.model = "iPhoneX";
 iPhone.platform_version = "11.3";
 iPhone.weight = 5;
 iPhone.price = 1200;
@@ -40,20 +41,50 @@ Samsung.platform_version = "Android P";
 Samsung.weight =  7;
 Samsung.price = 1100;
 
+Blackberry.model = "BlackberryZ10";
+Blackberry.platform_version = " Android N";
+Blackberry.weight = 6;
+Blackberry.price = 600;
+
 string customer_choice;
+double customer_max_price;
 
-cout<<"Please enter your choice of a smartphone with no spaces in between words." <<'\n';
-cin>>customer_choice;
+cout<< "Please enter your maximum price."<<'\n';
+cin>>customer_max_price;
 
-if (customer_choice == iPhone.model){
-    cout << "We have that in stock, it'll be "<<iPhone.price<< " CAD for an iPhone X." <<'\n';
-}
-else if (customer_choice == Samsung.model){
-    cout <<"We have that in stock, it'll be "<<Samsung.price<< " CAD for a Samsung Galaxy Note 8."<<'\n';
-}
-else{
-	cout<<"We are sorry we don\'t have that in stock this week. Please do return next week."<<'\n';
-}
+
+
+	if ((customer_max_price < Samsung.price) && (customer_max_price <iPhone.price) && (customer_max_price < Blackberry.price)){
+
+		cout<<"we are sorry, we do not have phones whose prices are less than "<<customer_max_price<<" CAD. Our minimum price is 600 CAD before tax "<<'\n';
+
+	}
+
+	else
+	{
+
+		cout<<"Please enter your model choice from any of the provided options with no spaces in between characters: (Note8, iPhoneX or BlackberryZ10)" <<'\n';
+		cin>>customer_choice;
+
+		if (customer_choice == iPhone.model){
+    		cout << "We have that in stock, it'll be "<<iPhone.price<< " CAD for an iPhone X." <<'\n';
+		}
+		else if (customer_choice == Samsung.model){
+    		cout <<"We have that in stock, it'll be "<<Samsung.price<< " CAD for a Samsung Galaxy Note 8."<<'\n';
+		}
+		else if(customer_choice == Blackberry.model){
+			cout<<"We have that in stock, it'll be "<<Blackberry.price<<" CAD for a Blackberry Z10."<<'\n';
+
+		}
+		else{
+			cout<<"We are sorry we don\'t have that in stock this week. Please do return next week."<<'\n';
+		}
+	}
+	
+
 return 0;
 }
+
+
+
 
