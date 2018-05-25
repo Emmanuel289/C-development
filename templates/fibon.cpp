718ef3ad2a4c9b9c6e592dea91a495b5805459f4
Okechukwu Ochia <emmirald@gmail.com>
2018-05-24 23:50:23 -0600

    modified the input argument to the fib function so it is specified as user input instead

--- templates/fibon.cpp
+++ templates/fibon.cpp
@@ -8,22 +8,21 @@ void fib_sequence_prod(int  array_length){
 
     int i;
     int fib_array[array_length];
-    float prod =1;
+    double prod =1;
 
 
     for (i=0; i <array_length; i++){
 
         if (i==0 ||i==1){
             fib_array[i] = 1;
-            cout<<fib_array[i]<<" ";
-            prod = prod*fib_array[i];
+			prod = prod*fib_array[i];
         }
 
 
             else {
                 fib_array[i] = fib_array[i-1] + fib_array[i-2];
                 prod = prod*fib_array[i];
-                cout<<fib_array[i]<<" ";
+                
 
             }
 
@@ -32,8 +31,8 @@ void fib_sequence_prod(int  array_length){
 
     }
 
-
-cout<<'\n'<<"The product of the first twenty elements of the fibonacci sequence above is: "<<prod<<endl;
+cout<<"The length of the array is: "<<array_length<<'\n';
+cout<<'\n'<<"The product of the first "<<array_length <<" elements of the fibonacci sequence above is: "<<prod<<endl;
 
 }
 
@@ -42,8 +41,11 @@ cout<<'\n'<<"The product of the first twenty elements of the fibonacci sequence 
 int main()
 {
 
+int length;
+cout <<"Enter the length of the array of numbers"<<'\t'; 
+cin>>length;
 
-    fib_sequence_prod(20);
+    fib_sequence_prod(length);
 
 
 
